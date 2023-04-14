0440ef36e200d596c8d0b7593637d69ba2b1254c
freundp99 <a11919391@unet.univie.ac.at>
2023-04-14 14:37:02 +0200

    logger output changed

--- task2/src/main/java/Client/Main.java
+++ task2/src/main/java/Client/Main.java
@@ -4,7 +4,6 @@ import Message.MessageObject;
 
 import java.net.*;
 import java.io.*;
-//import java.util.logging.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -12,7 +11,6 @@ import org.apache.logging.log4j.Logger;
 
 public class Main {
 
-    //private static final Logger logger = Logger.getLogger(Server.Main.class.getName());
     private static final Logger logger = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
