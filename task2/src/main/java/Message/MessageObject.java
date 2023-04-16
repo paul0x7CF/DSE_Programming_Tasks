19bb8b133a8b5a0517057351882d1ce440c2d21f
freundp99 <a11919391@unet.univie.ac.at>
2023-04-16 16:27:22 +0200

    Task2/ make messageClass serializable

--- task2/src/main/java/Message/MessageObject.java
+++ task2/src/main/java/Message/MessageObject.java
@@ -1,8 +1,9 @@
 package Message;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class MessageObject {
+public class MessageObject implements Serializable {
 
     private String name;
 
