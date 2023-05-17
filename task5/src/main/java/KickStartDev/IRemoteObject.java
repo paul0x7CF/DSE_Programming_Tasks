cf4f629b29ca2a3157d9f74d5408bb58026eeaaf
freundp99 <a11919391@unet.univie.ac.at>
2023-05-17 13:25:12 +0200

    task5/interface of remote object changed

--- task5/src/main/java/KickStartDev/IRemoteObject.java
+++ task5/src/main/java/KickStartDev/IRemoteObject.java
@@ -2,10 +2,11 @@ package KickStartDev;
 
 public interface IRemoteObject {
 
-    public void logSingleEntry(LogEntry entry);
+    public void singleLog(LogEntry entry);
     public void removeOldLogs(int amountToRemove);
+    public void increaseStorageSpace(int increaseBy);
     public void addLogsInBulk(LogEntry[] logBulk);
-    public LogEntry[] search(String searchTerm);
+    public LogEntry[] searchLogs(String searchTerm);
 
 
 }
