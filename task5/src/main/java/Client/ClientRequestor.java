cc4d70fdc0df36c79ef73644bcf697f7451052c7
freundp99 <a11919391@unet.univie.ac.at>
2023-05-17 15:33:00 +0200

    task5/invoke method added

--- task5/src/main/java/Client/ClientRequestor.java
+++ task5/src/main/java/Client/ClientRequestor.java
@@ -1,6 +1,12 @@
 package Client;
 
+import KickStartDev.RequestMessage;
+
 public class ClientRequestor {
 
     //The Class handles the requests to the Remote Object on the server
+    public static void invokeObjectByFireAndForget(RequestMessage requestMessage) throws Exception {
+        ClientRequestHandler.sendMessage(requestMessage.marshall());
+
+    }
 }
