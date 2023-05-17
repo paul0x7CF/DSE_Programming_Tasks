4299334edbcbe360fbc56db7c4bfaf8a2362bc78
freundp99 <a11919391@unet.univie.ac.at>
2023-05-17 13:26:43 +0200

    task5/interface methods added

--- task5/src/main/java/Client/ClientProxy.java
+++ task5/src/main/java/Client/ClientProxy.java
@@ -1,4 +1,31 @@
 package Client;
 
-public class ClientProxy {
+import KickStartDev.IRemoteObject;
+import KickStartDev.LogEntry;
+
+public class ClientProxy implements IRemoteObject {
+    @Override
+    public void singleLog(LogEntry entry) {
+
+    }
+
+    @Override
+    public void removeOldLogs(int amountToRemove) {
+
+    }
+
+    @Override
+    public void increaseStorageSpace(int increaseBy) {
+
+    }
+
+    @Override
+    public void addLogsInBulk(LogEntry[] logBulk) {
+
+    }
+
+    @Override
+    public LogEntry[] searchLogs(String searchTerm) {
+        return new LogEntry[0];
+    }
 }
