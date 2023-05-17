4362fc97ad28c9e1fd547aa5d6818f7245a0a40f
freundp99 <a11919391@unet.univie.ac.at>
2023-05-17 15:32:11 +0200

    task5/send methode added

--- task5/src/main/java/Client/ClientRequestHandler.java
+++ task5/src/main/java/Client/ClientRequestHandler.java
@@ -3,4 +3,8 @@ package Client;
 public class ClientRequestHandler {
 
     //The Class handled the network communication with the server
+    public static void sendMessage(byte[] marshall) {
+
+    }
+
 }
