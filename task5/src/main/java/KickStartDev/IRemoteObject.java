d45fb98691909cb310b30e2929c977e5c8f47e50
freundp99 <a11919391@unet.univie.ac.at>
2023-05-19 13:53:55 +0200

    task5/exception added

--- task5/src/main/java/KickStartDev/IRemoteObject.java
+++ task5/src/main/java/KickStartDev/IRemoteObject.java
@@ -2,7 +2,7 @@ package KickStartDev;
 
 public interface IRemoteObject {
 
-    public void singleLog(LogEntry entry);
+    public void singleLog(LogEntry entry) throws Exception;
     public void removeOldLogs(int amountToRemove);
     public void increaseStorageSpace(int increaseBy);
     public void addLogsInBulk(LogEntry[] logBulk);
