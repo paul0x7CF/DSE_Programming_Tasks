43f98544d30da3ed1ac1585afd7969b43079ba0c
freundp99 <a11919391@unet.univie.ac.at>
2023-05-19 14:20:37 +0200

    task5/used methode name changed

--- task5/src/main/java/Client/ClientRequestor.java
+++ task5/src/main/java/Client/ClientRequestor.java
@@ -6,7 +6,7 @@ public class ClientRequestor {
 
     //The Class handles the requests to the Remote Object on the server
     public static void invokeObjectByFireAndForget(RequestMessage requestMessage) throws Exception {
-        ClientRequestHandler.sendMessage(requestMessage.marshall());
+        ClientRequestHandler.sendMessageViaUDP(requestMessage.marshall());
 
     }
 }
