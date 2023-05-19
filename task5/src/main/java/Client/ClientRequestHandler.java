db3d8dcc263d6c3b4ad1fdfd0600f02376ec1e8c
freundp99 <a11919391@unet.univie.ac.at>
2023-05-19 14:21:49 +0200

    task5/logic to send Data implemented

--- task5/src/main/java/Client/ClientRequestHandler.java
+++ task5/src/main/java/Client/ClientRequestHandler.java
@@ -1,9 +1,32 @@
 package Client;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class ClientRequestHandler {
+    private static final Logger logger = LogManager.getLogger(ClientRequestHandler.class);
 
     //The Class handled the network communication with the server
-    public static void sendMessage(byte[] marshall) {
+    public static void sendMessageViaUDP(byte[] dataToSend) {
+
+        try {
+            //Create the socket to send the data
+            DatagramSocket clientSocket = new DatagramSocket();
+            InetAddress aHost = InetAddress.getByName("localhost");
+            int serverPort = 8080;
+            DatagramPacket request = new DatagramPacket(dataToSend, dataToSend.length, aHost, serverPort);
+            clientSocket.send(request);
+            logger.info("Data was sent to server over UDP connection");
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
