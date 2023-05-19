bfc4e0b1d34466658a6ea02fa3d8a5e005ee129a
freundp99 <a11919391@unet.univie.ac.at>
2023-05-19 13:55:02 +0200

    task5/used class name changed

--- task5/src/main/java/KickStartDev/RequestMessage.java
+++ task5/src/main/java/KickStartDev/RequestMessage.java
@@ -2,10 +2,10 @@ package KickStartDev;
 
 
 public class RequestMessage<T> implements IMarshall {
-	private final KnownMethods method;
+	private final EKnownMethods method;
 	private final T requestData;
 
-	public RequestMessage(KnownMethods method, T requestData) {
+	public RequestMessage(EKnownMethods method, T requestData) {
 		super();
 		this.method = method;
 		this.requestData = requestData;
