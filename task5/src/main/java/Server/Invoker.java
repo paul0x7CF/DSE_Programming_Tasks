23164112d18da3654d525ffda0ff8253d15c851f
freundp99 <a11919391@unet.univie.ac.at>
2023-05-20 16:15:16 +0200

    task5/Invoker logic implemented

--- task5/src/main/java/Server/Invoker.java
+++ task5/src/main/java/Server/Invoker.java
@@ -1,9 +1,51 @@
 package Server;
 
+import KickStartDev.IMarshall;
+import KickStartDev.LogEntry;
+import KickStartDev.RemoteObject;
+import KickStartDev.RequestMessage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Invoker {
 
-    public static byte[] invoke(byte[] data) {
-        //TODO: Implement this method
+    private static final Logger logger = LogManager.getLogger(Invoker.class);
+    private RemoteObject remoteObject;
+
+     public Invoker(RemoteObject remoteObject) {
+         this.remoteObject = remoteObject;
+     }
+
+    public byte[] invoke(byte[] data) {
+        try {
+            RequestMessage requestMessage = IMarshall.unmarshall(data);
+
+            switch (requestMessage.getMethod()) {
+                case singleLog -> {
+                    logger.debug("invoking singleLog method");
+                    remoteObject.logSingleEntry((LogEntry) requestMessage.getRequestData());
+                }
+                case removeOldLogs -> {
+                    logger.debug("invoking removeOldLogs method");
+                    //TODO: Implement this case
+                }
+                case increaseStorageSpace -> {
+                    logger.debug("invoking increaseStorageSpace method");
+                    //TODO: Implement this case
+                }
+                case addLogsInBulk -> {
+                    logger.debug("invoking addLogsInBulk method");
+                    //TODO: Implement this case
+                }
+                case searchLogs -> {
+                    logger.debug("invoking searchLogs method");
+                    //TODO: Implement this case
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 
